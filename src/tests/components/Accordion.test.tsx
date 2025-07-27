501bc834e00dd3a06f57987ca1dd6a26ae7fff84
bycarlosgamez <hardpx@gmail.com>
2025-07-27 13:56:21 -0600

    added accordion tests

--- src/tests/components/Accordion.test.tsx
+++ src/tests/components/Accordion.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 
 import {
   Accordion,
@@ -7,4 +7,38 @@ import {
   AccordionContent,
 } from '../../components';
 
-describe('Accordion Component', () => {});
+describe('Accordion Component', () => {
+  it('Should render an accordion component with one item', () => {
+    render(
+      <Accordion>
+        <AccordionItem>
+          <AccordionHeader>Accordion Title</AccordionHeader>
+          <AccordionContent>Content</AccordionContent>
+        </AccordionItem>
+      </Accordion>
+    );
+
+    expect(screen.getByText('Accordion Title')).toBeInTheDocument();
+
+    expect(screen.getByText('Content')).toBeInTheDocument();
+  });
+
+  it('Should toggle section content visibility', () => {
+    render(
+      <Accordion>
+        <AccordionItem>
+          <AccordionHeader>Accordion Title</AccordionHeader>
+          <AccordionContent>Content</AccordionContent>
+        </AccordionItem>
+      </Accordion>
+    );
+
+    const contentElement = screen.getByText('Content');
+
+    expect(contentElement).toHaveClass('invisible');
+
+    fireEvent.click(screen.getByText('Accordion Title'));
+
+    expect(contentElement).not.toHaveClass('invisible');
+  });
+});
